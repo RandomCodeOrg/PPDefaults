607868cb94101ebf6560b0f4a97aa9bf9166c3e6
Marcel Singer <marcel.singer@live.de>
2015-12-16 10:27:26 +0100

    Added validation annotations

---
+++ src/main/java/com/github/randomcodeorg/ppplugin/ppdefaults/validation/CheckNotNegative.java
@@ -0,0 +1,14 @@
+package com.github.randomcodeorg.ppplugin.ppdefaults.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface CheckNotNegative {
+
+	Class<? extends RuntimeException> value() default IllegalArgumentException.class;
+
+}
