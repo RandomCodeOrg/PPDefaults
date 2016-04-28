a862f953ac4bc628b729b223262e99019714d79a
Marcel Singer <marcel.singer@live.de>
2016-04-28 16:05:28 +0200

    Completed the documentation for BindingSource

--- plugin/src/main/java/com/github/randomcodeorg/ppplugin/ppdefaults/binding/BindingSource.java
+++ plugin/src/main/java/com/github/randomcodeorg/ppplugin/ppdefaults/binding/BindingSource.java
@@ -37,9 +37,9 @@ public interface BindingSource extends BindingSourceFactory {
 	 * <p>
 	 * <b>Note:</b> The supported value types depend on the specific
 	 * implementation. Please refer to the appropriate documentation to learn
-	 * more about the types supported by the used implementation. Although every
-	 * implementation should at least (by convention) support the primitive
-	 * types (e.g. byte, int, long, double, boolean) and strings.
+	 * more about this issue. Although every implementation should at least (by
+	 * convention) support the primitive types (e.g. byte, int, long, double,
+	 * boolean) and strings.
 	 * </p>
 	 * 
 	 * @param key
@@ -63,21 +63,91 @@ public interface BindingSource extends BindingSourceFactory {
 	 */
 	public <T> void set(String key, T value) throws IllegalArgumentException;
 
-	public boolean contains(String key) throws NoSuchElementException, IllegalArgumentException;
+	/**
+	 * Returns <code>true</code> if there is a value for the specified key.
+	 * 
+	 * @param key
+	 *            The key to check.
+	 * @return <code>true</code> if there is a value for the specified key.
+	 * @throws IllegalArgumentException
+	 *             Is thrown if the given key is invalid (e.g. <code>null</code>
+	 *             or an empty string). Every implementation can specify its own
+	 *             requirements regarding the allowed key values. One may refer
+	 *             to the corresponding documentation.
+	 */
+	public boolean contains(String key) throws IllegalArgumentException;
 
+	/**
+	 * Removes the key and its associated value.
+	 * 
+	 * @param key
+	 *            The key to be removed.
+	 * @return <code>true</code> if the key was present before this method was
+	 *         called. <code>false</code> otherwise.
+	 * @throws IllegalArgumentException
+	 *             Is thrown if the given key is invalid (e.g. <code>null</code>
+	 *             or an empty string). Every implementation can specify its own
+	 *             requirements regarding the allowed key values. One may refer
+	 *             to the corresponding documentation.
+	 */
 	public boolean remove(String key) throws NoSuchElementException, IllegalArgumentException;
 
+	/**
+	 * Loads this store. All implementations of this interface are required to
+	 * tolerate multiple invocations of this method. Only the first call to this
+	 * method should actually perform the necessary operations. No action should
+	 * be done on subsequent invocations.
+	 */
 	public void load();
 
+	/**
+	 * Flushes all changes by writing them to a persistent medium.
+	 */
 	public void flush();
 
+	/**
+	 * Flushes all changes by writing them to a persistent medium and releases
+	 * all associated resources.
+	 */
 	public void close();
 
-	public void registerListener(String key, BindingListener listener)
-			throws NoSuchElementException, IllegalArgumentException;
+	/**
+	 * Registers a listener.
+	 * 
+	 * @param key
+	 *            The key of the key/value pair to listen for.
+	 * @param listener
+	 *            The listener that should be notified when the specified value
+	 *            changes.
+	 * @throws IllegalArgumentException
+	 *             Is thrown if the given key is invalid (e.g. <code>null</code>
+	 *             or an empty string). Every implementation can specify its own
+	 *             requirements regarding the allowed key values. One may refer
+	 *             to the corresponding documentation.
+	 */
+	public void registerListener(String key, BindingListener listener) throws IllegalArgumentException;
 
+	/**
+	 * Removes the given listener.
+	 * 
+	 * @param key
+	 *            The key of the key/value pair to unregister from.
+	 * @param listener
+	 *            The listener to unregister.
+	 * @throws IllegalArgumentException
+	 *             Is thrown if the given key is invalid (e.g. <code>null</code>
+	 *             or an empty string). Every implementation can specify its own
+	 *             requirements regarding the allowed key values. One may refer
+	 *             to the corresponding documentation.
+	 */
 	public void releaseListener(String key, BindingListener listener) throws IllegalArgumentException;
 
+	/**
+	 * Returns this instance.
+	 * @param identifier <p style="text-decoration: line-through">The identifier to be returned.</p>
+	 * <p><b>This parameter is ignored.</b></p>
+	 * @return This method will <b>always</b> (regardless of the given parameter) return this instance.
+	 */
 	@Override
 	BindingSource getBindingSource(String identifier);
 
--- plugin/src/main/java/com/github/randomcodeorg/ppplugin/ppdefaults/binding/BindingSourceFactory.java
+++ plugin/src/main/java/com/github/randomcodeorg/ppplugin/ppdefaults/binding/BindingSourceFactory.java
@@ -3,6 +3,11 @@ package com.github.randomcodeorg.ppplugin.ppdefaults.binding;
 public interface BindingSourceFactory {
 	
 	
+	/**
+	 * Returns the binding source for the given identifier.
+	 * @param identifier The identifier of the binding source to be returned.
+	 * @return The binding source for the given identifier.
+	 */
 	BindingSource getBindingSource(String identifier);
 
 	
