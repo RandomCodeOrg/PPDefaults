3f95556e51c86ec72d1c9361f31b5f469db6c255
Marcel Singer <marcel.singer@live.de>
2016-06-06 17:15:59 +0200

    Removed duplicate/obsolete classes

--- plugin/src/main/java/com/github/randomcodeorg/ppplugin/ppdefaults/binding/BindingListener.java
+++
@@ -1,9 +0,0 @@
-package com.github.randomcodeorg.ppplugin.ppdefaults.binding;
-
-public interface BindingListener {
-
-	void bindingRemoved(BindingSource source, String key);
-	
-	<T> void valueUpdated(BindingSource source, String key, T oldValue, T newValue);
-	
-}
